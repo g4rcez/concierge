30ce044b5e886ddc1e72ac6cf700bccad44cef48
g4rcez <allan.f.garcez@gmail.com>
2022-01-14 23:26:34 -0300

    feat: application hub

---
+++ concierge/src/concierge/applications.ts
@@ -0,0 +1,112 @@
+import { Request, Response, Router } from "express";
+import { join } from "path";
+import { Optimization, OptimizedJS } from "../frontend/optimization";
+import { Storage } from "../storage";
+import { Strings } from "../strings";
+import { Http } from "./http";
+
+export enum ApplicationType {
+  orchestrator = "orchestrator",
+  app = "app",
+}
+
+enum EntrypointType {
+  orchestrator = "/index.html",
+  app = "/index.js",
+}
+
+enum Bundler {
+  viteRollup = "viteRollup",
+}
+
+export type FrontendConfig = {
+  route: string;
+  origin: string;
+  version: string;
+  roles: string[];
+  bundler: Bundler;
+  disabled: boolean;
+  hotReload: boolean;
+  type: ApplicationType;
+};
+
+const rootAlias = "@concierge/entrypoint";
+
+export type ApplicationFile = Storage.File & { path: string };
+
+export class Application {
+  private map = new Map<string, ApplicationFile>();
+
+  constructor(public app: FrontendConfig, public version = app.version, public name = app.route, public disabled = app.disabled) {}
+
+  public baseUrl = (...paths: string[]) => join(this.app.route, this.app.version, ...paths).replace(/^\/+/, "/");
+
+  public getVendorFiles = () => {
+    const files = new Map<string, ApplicationFile>();
+    this.map.forEach((content, key) => {
+      if (key.includes("@vendor")) {
+        files.set(key, content);
+      }
+    });
+    return files;
+  };
+
+  public removeFiles = (files: string[]) => {
+    files.forEach((file) => this.map.delete(file));
+  };
+
+  public load = async (storage: Storage.Interface) => {
+    const allFiles = await storage.listAllFiles(this.app);
+    const base = Strings.url(this.app.route, this.app.version);
+    await Promise.all(
+      allFiles.map(async (name) => {
+        const [, path] = name.split(base);
+        const file = await storage.file(path, this.app);
+        const mappedFile = { content: file.content, contentType: file.contentType, path };
+        if (this.app.type === ApplicationType.orchestrator && path === EntrypointType.orchestrator) {
+          this.map.set(rootAlias, mappedFile);
+          return;
+        }
+        if (this.app.type === ApplicationType.app && path === EntrypointType.app) {
+          this.map.set(rootAlias, mappedFile);
+          return;
+        }
+        this.map.set(path, mappedFile);
+      })
+    );
+  };
+
+  public optimize = async (commonDependencies: OptimizedJS, transformContent: Optimization["replaceJsImports"]) => {
+    commonDependencies.forEach((js) => this.map.delete(js.path));
+    const baseUrl = this.baseUrl();
+    await Promise.all(
+      Array.from(this.map.keys()).map(async (item) => {
+        const file = this.map.get(item)!;
+        file.content = await transformContent(file.content, baseUrl);
+      })
+    );
+  };
+
+  public getEntryPoint = () => {
+    const entry = this.map.get(rootAlias)!;
+    return { content: entry.content, type: this.app.type };
+  };
+
+  public serve = async (router: Router, newEntryPoint: string) => {
+    this.map.forEach((file, key) => {
+      if (key === rootAlias) {
+        const path = `/${this.app.route}((/[A-Za-z0-9_-]+/?)+|/?)`;
+        const sendEntrypoint = (req: Request, res: Response) => {
+          res.contentType(file.contentType);
+          return res.send(newEntryPoint);
+        };
+        router.get(`/${this.app.route}((/[A-Za-z0-9_-]+/?)+|/)`, sendEntrypoint);
+        router.all(path, sendEntrypoint);
+      } else {
+        const path = `/${this.baseUrl(file.path)}`;
+        router.get(path, (_, res) => res.contentType(file.contentType).send(file.content));
+        router.all(path, (_, res) => res.sendStatus(Http.StatusCode.MethodNotAllowed));
+      }
+    });
+  };
+}
