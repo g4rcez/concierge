6dd580f82b73c4fe2a2b9a607a2ad106e2eee406
g4rcez <allan.f.garcez@gmail.com>
2022-01-14 23:26:42 -0300

    feat: http module abstract

---
+++ concierge/src/concierge/http.ts
@@ -0,0 +1,11 @@
+export namespace Http {
+  export enum StatusCode {
+    MethodNotAllowed = 405,
+    InternalError = 500,
+    NotFound = 404,
+    Forbidden = 403,
+    NotAuthorized = 401,
+    Unavailable = 503,
+    Ok = 200,
+  }
+}
